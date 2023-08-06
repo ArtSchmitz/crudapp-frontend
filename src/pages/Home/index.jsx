801057c33a3c1c6519bfe6681c54da60b8e6fc40
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-08-06 18:41:54 -0300

    feat axios and integrate API

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Home = () => {
-    
-    return(
-        <h1>Home</h1>
-    )
-}
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const getBook = () => {
+      axios
+        .get("http://127.0.0.1:8000/api/books")
+        .then((response) => {
+          console.log(response.data);
+          setBook(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getBook();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de livros</h1>
+      <ul>
+        {book.map((book) => (
+          <li key={book.id}>
+            {book.title} - {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
