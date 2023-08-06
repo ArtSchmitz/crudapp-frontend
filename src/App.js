f45ff1e734666680d7dac8769df72dcd9d05fee0
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-08-06 19:07:56 -0300

    upps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Home } from './pages/Home';
-import { Cadastrar } from './pages/Cadastrar';
-import { Atualizar } from './pages/Atualizar';
+import { Home } from "./pages/Livros";
+import { Cadastrar } from "./pages/Cadastrar";
+import { Atualizar } from "./pages/Atualizar";
+import Footer from "./pages/Home/Footer";
+import Header from "./pages/Home/Header";
 
 function App() {
   return (
     <div>
-    <Router>
-      <Switch>
+      <Router>
+        <Header />
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/cadastrar" component={Cadastrar} />
           <Route exact path="/atualizar" component={Atualizar} />
-      </Switch>
-    </Router>
-  </div>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
