ff19719ff1e8a79df457531e90ceed2aab7b9932
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-08-06 21:42:03 -0300

    upp

--- src/pages/Cadastrar/index.jsx
+++ src/pages/Cadastrar/index.jsx
@@ -5,6 +5,7 @@ export const Cadastrar = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [publisher, setPublisher] = useState("");
+  const [message, setMessage] = useState("");
 
   const newBook = {
     title: title,
@@ -16,10 +17,11 @@ export const Cadastrar = () => {
     await axios
       .post("http://127.0.0.1:8000/api/book", newBook)
       .then((response) => {
-        console.log("Livro cadastrado com sucesso", response.data);
+        console.log(response.data.message, response.data);
         setTitle("");
         setAuthor("");
         setPublisher("");
+        setMessage(response.data.message);
       })
       .catch((error) => {
         console.error("Erro ao cadastrar o livro", error);
@@ -31,54 +33,71 @@ export const Cadastrar = () => {
     await addBook();
   };
 
-  return (
-<form onSubmit={handleSubmit} className="mt-4 p-4 bg-white shadow-lg rounded-lg">
-  <div className="mb-3">
-    <label htmlFor="title" className="form-label">
-      Título
-    </label>
-    <input
-      type="text"
-      className="form-control"
-      id="title"
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      placeholder="Insira o título do livro"
-      required
-    />
-  </div>
-  <div className="mb-3">
-    <label htmlFor="author" className="form-label">
-      Autor
-    </label>
-    <input
-      type="text"
-      className="form-control"
-      id="author"
-      value={author}
-      onChange={(e) => setAuthor(e.target.value)}
-      placeholder="Insira o nome do autor"
-      required
-    />
-  </div>
-  <div className="mb-3">
-    <label htmlFor="publisher" className="form-label">
-      Editora
-    </label>
-    <input
-      type="text"
-      className="form-control"
-      id="publisher"
-      value={publisher}
-      onChange={(e) => setPublisher(e.target.value)}
-      placeholder="Insira o nome da editora"
-      required
-    />
-  </div>
-  <button type="submit" className="btn btn-primary text-center">
-    Cadastrar
-  </button>
-</form>
+  useEffect(() => {
+    let timer;
+    if (message) {
+      timer = setTimeout(() => {
+        setMessage("");
+      }, 2000); 
+    }
+    return () => clearTimeout(timer);
+  }, [message]);
 
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="mt-4 p-4 bg-white shadow-lg rounded-lg"
+    >
+      {message && (
+        <div className="alert alert-success mb-3" role="alert">
+          {message}
+        </div>
+      )}
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Título
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Insira o título do livro"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="author" className="form-label">
+          Autor
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Insira o nome do autor"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="publisher" className="form-label">
+          Editora
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="publisher"
+          value={publisher}
+          onChange={(e) => setPublisher(e.target.value)}
+          placeholder="Insira o nome da editora"
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary text-center">
+        Cadastrar
+      </button>
+    </form>
   );
 };
